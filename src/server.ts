0f37aba5d6b059470d5cb3e3b18b5e15f2074a45
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-11-04 13:17:40 -0300

    actualizacion de routers para nuevas tareas

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import secureJwtRouter from "./routes/secure-jwt";
 import secureBasicRouter from "./routes/secure-basic";
 import mainRouter from "./routes/main-route";
 import labRouter from './routes/lab-route';
+import lab2Router from './routes/lab2-route';
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use('/secure-key', secureRouter);
 app.use('/secure-basic', secureBasicRouter);
 app.use('/secure-jwt', secureJwtRouter);
 app.use('/api', labRouter);
+app.use('/lab-2', lab2Router);
 
 export default app;
