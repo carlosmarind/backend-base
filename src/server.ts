c2f0a0d0e396d4f3edbe1f0c4b43655bffd512cd
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-01-27 09:09:44 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import secureRouter from "./routes/secure-key.js";
 import secureJwtRouter from "./routes/secure-jwt.js";
 import secureBasicRouter from "./routes/secure-basic.js";
 import mainRouter from "./routes/main-route.js";
-import labRouter from './routes/lab-route.js';
-import lab2Router from './routes/lab2-route.js';
 
 const app = express();
 
@@ -16,7 +14,5 @@ app.use('/', mainRouter);
 app.use('/secure-key', secureRouter);
 app.use('/secure-basic', secureBasicRouter);
 app.use('/secure-jwt', secureJwtRouter);
-app.use('/api', labRouter);
-app.use('/lab-2', lab2Router);
 
 export default app;
